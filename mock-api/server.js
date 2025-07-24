af3da1652c96683b8ee8be802c45be7752af3fcf
DinhHieu2506 <hieucr78@gmail.com>
2025-07-24 10:40:26 +0700

    update server

--- mock-api/server.js
+++ mock-api/server.js
@@ -1,10 +1,18 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+// CORS config
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
+
+server.use(middlewares);
+server.use(router);
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running');
+});
